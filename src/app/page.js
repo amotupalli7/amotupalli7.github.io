5527f0d96525b32cd9d7a42bc4bbdc0c7bb58582
amotupalli7 <sspmam@gmail.com>
2023-06-20 15:07:46 -0400

    dark mode still needs to be fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
+
 'use client'
 import Head from "next/head";
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from "react-icons/ai";
-import { useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import deved from "C:/Users/amotupalli/Desktop/PortfolioWebsite/public/dev-ed-wave.png"
 import design from "C:/Users/amotupalli/Desktop/PortfolioWebsite/public/design.png"
@@ -15,18 +16,19 @@ import web3 from "C:/Users/amotupalli/Desktop/PortfolioWebsite/public/web3.png"
 import web4 from "C:/Users/amotupalli/Desktop/PortfolioWebsite/public/web4.png"
 import web5 from "C:/Users/amotupalli/Desktop/PortfolioWebsite/public/web5.png"
 import web6 from "C:/Users/amotupalli/Desktop/PortfolioWebsite/public/web6.png"
+import { Damion } from "next/font/google";
 
 
 export default function Home() {
   const [darkMode,setDarkMode] = useState(false)
   return (
     <div classname = {darkMode ? "dark" : ""}>
-    <main className=" bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+    <main className=" bg-gray-100 px-10 md:px-20 lg:px-40 dark:bg-gray-900">
       <section className="min-h-screen ">
         <nav className="py-10 mb-12 flex justify-between">
           <h1 className=" text-xl font-burtons">Developedbyabhi</h1>
           <ul className="flex items-center">
-            <li><BsFillMoonStarsFill onclick = {() => setDarkMode(!darkMode)}  className=" cursor-pointer text-2xl"/> </li>
+            <li><BsFillMoonStarsFill onClick= {()=> !darkMode}  className=" cursor-pointer text-2xl"/> </li>
             <li><a href="#" className=" bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8">Resume</a></li>
           </ul>
         </nav>
@@ -102,11 +104,11 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-          <div classname=" basis-1/3 flex-1"><Image src={web2} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
-          <div classname=" basis-1/3 flex-1"><Image src={web1} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
-          <div classname=" basis-1/3 flex-1"><Image src={web3} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
+          <div classname=" basis-1/3 flex-1"><Image src={web3} className=" rounded-lg object-cover" width={"100%"} height={"80%"} layout="responsive"></Image></div>
           <div classname=" basis-1/3 flex-1"><Image src={web4} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
+          <div classname=" basis-1/3 flex-1"><Image src={web2} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
           <div classname=" basis-1/3 flex-1"><Image src={web5} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
+          <div classname=" basis-1/3 flex-1"><Image src={web1} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
           <div classname=" basis-1/3 flex-1"><Image src={web6} className=" rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"></Image></div>
 
         </div>
